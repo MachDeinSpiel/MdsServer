77cd9077a29f544a828266b1d701c94526e7ee7b
truefalse01 <ingmar.wittkau@googlemail.com>
2014-05-14 16:41:46 +0200

    Kommunikations Implementation

--- MdsServer/src/de/hsbremen/mds/server/domain/MdsComServer.java
+++ MdsServer/src/de/hsbremen/mds/server/domain/MdsComServer.java
@@ -2,20 +2,16 @@ package de.hsbremen.mds.server.domain;
 
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 
 import org.java_websocket.WebSocket;
 import org.java_websocket.framing.Framedata;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
-import org.json.JSONObject;
 
 import de.hsbremen.mds.common.communication.EntryHandler;
 import de.hsbremen.mds.common.exception.UnknownWhiteboardTypeException;
 import de.hsbremen.mds.common.interfaces.ComServerInterface;
-import de.hsbremen.mds.common.whiteboard.Whiteboard;
 import de.hsbremen.mds.common.whiteboard.WhiteboardEntry;
 import de.hsbremen.mds.common.whiteboard.WhiterboardUpdateObject;
 
@@ -25,13 +21,7 @@ import de.hsbremen.mds.common.whiteboard.WhiterboardUpdateObject;
 public class MdsComServer extends WebSocketServer implements ComServerInterface {
 	
 	private MdsServerInterpreter mdsServerInterpreter = new MdsServerInterpreter(this);
-	
-	private HashMap<Integer,JSONObject> locat = new HashMap<Integer, JSONObject>();
-	private HashMap<Integer,WebSocket> clients = new HashMap<Integer, WebSocket>();
-	private int idcount;
-	
-
-	
+		
 	
 	public MdsComServer(int port) throws UnknownHostException {
 		super(new InetSocketAddress(port) );
@@ -44,34 +34,27 @@ public class MdsComServer extends WebSocketServer implements ComServerInterface 
 
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
-		this.mdsServerInterpreter.addNewClient(conn);
+		/*this.mdsServerInterpreter.addNewClient(conn);
 		clients.put(idcount++, conn);
 		this.sendToAll("new connection: " + handshake.getResourceDescriptor() );
 		System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+		*/
+	
+		boolean isConnected = this.onNewConnection(conn, handshake);
+		
+		if(!isConnected) {
+			conn.close(0);
+		}
 	}
 
 	@Override
 	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-		this.mdsServerInterpreter.removeClient(conn, code, reason, remote);
-		this.sendToAll(conn + " has left the room!");
-		System.out.println( conn + " has left the room!");
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, String message) {
 		
 		WhiterboardUpdateObject wObj = EntryHandler.toObject(message);
-		 
-		
-		/*
-		for(Entry<Integer, WebSocket> entry: this.clients.entrySet()){
-			  if (!entry.getValue().equals(conn)) {
-				  entry.getValue().send(message);
-			  }
-			
-		}
-		*/
-			
 		mdsServerInterpreter.onWhiteboardUpdate(conn, wObj.getKeys(), wObj.getValue());
 		
 		System.out.println(conn + ": " + message);
@@ -92,72 +75,26 @@ public class MdsComServer extends WebSocketServer implements ComServerInterface 
 		}
 	}
 
-	/**
-	 * Sends <var>text</var> to all currently connected WebSocket clients.
-	 * 
-	 * @param text
-	 *            The String to send across the network.
-	 * @throws InterruptedException
-	 *             When socket related I/O errors occur.
-	 */
-	public void sendToAll(String text) {
-		Collection<WebSocket> con = connections();
-		synchronized (con) {
-			for(WebSocket c : con) {
-				c.send(text);
-			}
-		}
+	@Override
+	public boolean onNewConnection(WebSocket conn, ClientHandshake handshake) {
+		return mdsServerInterpreter.onNewConnection(conn, handshake);
 	}
-	
-	public void notifyWSClients(String object, int id, String status) {
-		JSONObject json = new JSONObject();
-		json.put(object, id);
-		json.put("status", status);
-		this.sendToAll(json.toString());
-	}
-
 
 	@Override
-	/**
-	 * Wird vom Interpreter aufgerufen wenn es ein WhiteboardUpdate gibt
-	 * @prama WebSocket conn - Client dem das Update mitgeteilt werden soll
-	 * @prama List<String> keys - Pfad zum WhiteboardEntry
-	 * @prama WhiteboardEntry entry - der WhiteboardEntry der dem Client mitgeteilt werden soll
-	 */
-	public void onWhiteboardUpdate(WebSocket conn, List<String> keys, WhiteboardEntry entry) {
+	public void onWhiteboardUpdate(WebSocket conn, List<String> keys, WhiteboardEntry value) {
+		System.err.println("onWhitboardUpdate auf ComServer");
 		
-		String message = null;
+	}
+	
+	public void sendUpdate(WebSocket conn, List<String> keys, WhiteboardEntry entry) {
+		String message = "";
 		try {
 			message = EntryHandler.toJson(keys, entry);
 		} catch (UnknownWhiteboardTypeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		conn.send(message);
-		
-	}
-
-	
-	
-	// Bot zum Senden zufälliger Koordninaten über WS
-	public void bot(){
-		double botLongitude, botLatitude;
-		while(true){
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			botLatitude = myRandom(52.9,53.9);
-			botLongitude = myRandom(8.2,8.9);
-			System.out.println("Latitude: " +botLatitude+ " und Longitude: " +botLongitude);
-		}
-	}
-	// Zufallsgenerator
-	public static double myRandom(double low, double high) {
-		return Math.random() * (high - low) + low;
 	}
 
 
--- MdsServer/src/de/hsbremen/mds/server/domain/MdsServerInterpreter.java
+++ MdsServer/src/de/hsbremen/mds/server/domain/MdsServerInterpreter.java
@@ -3,92 +3,83 @@ package de.hsbremen.mds.server.domain;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Vector;
 import java.util.regex.Pattern;
+
 import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+
+import de.hsbremen.mds.common.interfaces.ComServerInterface;
 import de.hsbremen.mds.common.interfaces.ServerInterpreterInterface;
-import de.hsbremen.mds.common.valueobjects.MdsImage;
-import de.hsbremen.mds.common.valueobjects.MdsItem;
-import de.hsbremen.mds.common.valueobjects.MdsMap;
-import de.hsbremen.mds.common.valueobjects.MdsText;
-import de.hsbremen.mds.common.valueobjects.MdsVideo;
 import de.hsbremen.mds.common.whiteboard.Whiteboard;
 import de.hsbremen.mds.common.whiteboard.WhiteboardEntry;
 
-public class MdsServerInterpreter implements ServerInterpreterInterface {
+public class MdsServerInterpreter implements ServerInterpreterInterface, ComServerInterface {
 	//Test Whiteboard
 	Whiteboard whiteboard = new Whiteboard();
 	//---------------------------------
 	private MdsComServer comServer;
 	//Websockets Hashmap...
-	private HashMap<Integer,WebSocket> clients = new HashMap<Integer, WebSocket>();
-	private int idcount = 0;
+	private HashMap<String,WebSocket> clients = new HashMap<String, WebSocket>();
+
 	
 	public MdsServerInterpreter (MdsComServer mdsComServer) {
 		this.comServer = mdsComServer;
 		
 	}
-	
 
-//	@Override
-//	public void onWhiteboardUpdate(List<String> keys, WhiteboardEntry value) {
-//		// TODO Auto-generated method stub
-//		
-//	}
 	
-	public void onWhiteboardUpdate(String path, String value) {
-		String[] keys = getStringWhiteboardPath(path);
-		//Object obValue = getWhiteboardValue(value);
-		if(whiteboard.getAttribute(keys) != null){
-			whiteboard.setAttributeValue(value, keys);
-		}else if(whiteboard.getAttribute(keys) == null){
-			whiteboard.setAttributeValue(value, keys);
-		}
-		
-		
-		//TODO: nach erfolgreichen update, MdsComServer die Strings uebergebene 
-	}
-	/*
-	 * Zerlegt den String Path
-	 * @return: String Array 
-	 */
-	private String[] getStringWhiteboardPath(String path){
-		String[] pathArray = path.split(Pattern.quote("+"));
-		return pathArray;
-	}
-	
-	//
-	public void receiveMessage(Object ob, WebSocket ws){
-		
-	}
-	//Client merken
-	public void addNewClient(WebSocket conn) {
-		if (!this.clients.containsValue(conn)) {
-			this.clients.put(this.idcount++, conn);
-		}
-		//TODO: whiteboard update?!
-	}
-
-	//Client entfernen
-	public void removeClient(WebSocket conn, int code, String reason, boolean remote) {
-		if(this.clients.containsValue(conn)){
-			for (Map.Entry<Integer, WebSocket> entry : clients.entrySet()){
-	    		if(entry.getValue().equals(conn)){
-	    			this.clients.remove(entry.getKey());
-	    		}
-	    	}
-		}
-	}
 
 	@Override
 	/**
-	 * Wenn vom ComServer eine neue Nachricht empfangen wird, wird diese Methode aufgerufen.
-	 * 
-	 * @param WebSocket conn - das WebSocket welches die Nachricht gesendet hat
-	 * @param List<String> keys - Der Pfad als List
-	 * @param WhiteboardEntry entry - der Entry mit Value und Visibility
 	 */
 	public void onWhiteboardUpdate(WebSocket conn, List<String> keys, WhiteboardEntry entry) {
-		// TODO Auto-generated method stub
 		
+		// Lokales WB aktualisieren
+		this.onWhiteboardUpdate(keys, entry);
+		
+		// Allen anderen Clients das Update schicken
+		for (Entry<String, WebSocket> mapEntry : this.clients.entrySet()) {
+			if (mapEntry.getValue() != conn) {
+				this.comServer.sendUpdate(mapEntry.getValue(), keys, entry);
+			}
+		}
+		
+	}
+
+
+	@Override
+	public void onWhiteboardUpdate(List<String> keys, WhiteboardEntry value) {
+		String[] key = new String[keys.size()];
+		key = keys.toArray(key);
+		this.whiteboard.setAttribute(value, key);
+	}
+
+
+	@Override
+	public boolean onNewConnection(WebSocket conn, ClientHandshake handshake) {
+		
+		String playerName = handshake.getResourceDescriptor();
+		
+		WhiteboardEntry player = this.whiteboard.getAttribute("Players", playerName);
+		
+		if (player == null) {
+			player = new WhiteboardEntry(playerName, "all");
+			
+			
+			this.clients.put("Players," + playerName, conn);
+			
+			List<String> keys = new Vector<String>();
+			keys.add("Players");
+			keys.add(playerName);
+			
+			
+			this.onWhiteboardUpdate(conn, keys, player);
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
