07457df722743cfdbb330e8bc1b05bf29cf2c444
olive_000 <olive_000@172.31.14.229>
2014-05-28 12:54:09 +0200

    - Dynamische Tabelle eingefügt - Event hinzugefügt wenn Tabellenfeld angeklickt wird

---
+++ MdsServer/MonitoringTool/playerTable.js
@@ -0,0 +1,68 @@
+//var whiteboard = this.whiteboard;
+
+function delete_table()	{
+
+	if (document.getElementById("players_table")) {
+		$(document.getElementById("players_table")).remove();
+	}
+}
+
+function generate_table() {
+	
+	delete_table();
+	// get the reference for the body
+	var body = document.getElementsByTagName("body")[0];
+ 
+	// creates the <table> element and creates a <tbody> element
+	var tbl = document.createElement("table");
+	tbl.setAttribute('id','players_table');
+	tbl.setAttribute('border','2');
+	console.log(tbl);
+  var tblBody = document.createElement("tbody");
+  
+//  counter (entspricht anz Spieler) and table content
+  var tblPlayers = ["player1","p2","p3","p4"];
+  
+  // creating all cells
+  for (var i = 0; i < tblPlayers.length; i++) {
+    // creates a table row
+    var row = document.createElement("tr");
+ 
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      var cell = document.createElement("td");
+      var cellText = document.createTextNode(tblPlayers[i]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+ 
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+  
+ 
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  addRowHandlers();
+}
+
+function addRowHandlers() {
+	var table = document.getElementById("players_table");
+    var rows = table.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+    	var currentRow = table.rows[i];
+        var createClickHandler = function(row) {
+        	return function() {
+        		var cell = row.getElementsByTagName("td")[0];
+        		var id = cell.innerHTML;
+        		// Hier könnte der Aufruf für die Ausgabe stehen
+        		alert("id:" + id);
+        	};
+        };
+        
+        currentRow.onclick = createClickHandler(currentRow);
+        
+    }
+}
