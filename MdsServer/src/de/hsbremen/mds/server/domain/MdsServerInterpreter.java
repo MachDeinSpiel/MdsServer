e7d1333336bf8c81f714030b29ccfdab5832d3e4
jluziv <jluziv@hs-bremen.de>
2014-06-06 16:09:56 +0200

    richter path bei player update

--- MdsServer/src/de/hsbremen/mds/server/domain/MdsServerInterpreter.java
+++ MdsServer/src/de/hsbremen/mds/server/domain/MdsServerInterpreter.java
@@ -39,8 +39,6 @@ public class MdsServerInterpreter implements ComServerInterface {
 	 * 
 	 */
 	public void onWhiteboardUpdate(WebSocket conn, List<String> keys, WhiteboardEntry entry) {
-		//Whiteboard loeschen TODO:klappt noch nicht, update wird an alle spieler verschickt.
-		//Das kommt beim client an. '{"valuetype":"java.lang.String","visibility":"none","value":"delete","path":"Players,Hans","mode":"single"}'
 		if(entry.getValue().equals("delete")){
 			this.removeWhiteboard(conn, keys);
 			for (Entry<String, WebSocket> mapEntry : this.clients.entrySet()) {
@@ -54,12 +52,15 @@ public class MdsServerInterpreter implements ComServerInterface {
 			} catch (InvalidWhiteboardEntryException e) {
 				e.printStackTrace();
 			}
-			
+			//TODO: der path ist falsch... 
 			// Allen anderen Clients das Update schicken
 			for (Entry<String, WebSocket> mapEntry : this.clients.entrySet()) {
 				if (!mapEntry.getValue().equals(conn)) {
 					for(Iterator<WhiteboardUpdateObject> iter = this.whiteboardUpdateObjects.iterator(); iter.hasNext();){
-						this.comServer.sendUpdate(mapEntry.getValue(), keys, iter.next().getValue());
+						WhiteboardUpdateObject a = iter.next();
+						WhiteboardEntry b = a.getValue();
+						List<String> k = a.getKeys();
+						this.comServer.sendUpdate(mapEntry.getValue(), k, b);
 					}
 				}	
 			}
